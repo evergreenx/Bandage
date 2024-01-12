be09f65587984a0068250e9e670ef37eda973151
ido evergeen <dicksonevergreenido@gmail.com>
2024-01-12 18:32:21 +0100

    add setup files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 /* Components */
+import { Typography } from "@mui/material";
 import { Counter } from "./components/Counter/Counter";
 
+import Button from "@mui/material/Button";
+
 export default function IndexPage() {
-  return <Counter />;
+  return (
+    <>
+
+    <Typography>
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quos
+      numquam voluptates ipsa et repellendus doloribus odit. Ab quibusdam
+      voluptates aut perspiciatis minus alias in explicabo consectetur. Impedit,
+      a nesciunt!
+    </Typography>
+    
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
 }
 
 export const metadata = {
-  title: "Redux Toolkit",
+  title: "Bandage store || Home",
 };
--- lib/providers.tsx
+++ lib/providers.tsx
@@ -5,7 +5,19 @@ import { Provider } from "react-redux";
 
 /* Instruments */
 import { reduxStore } from "@/lib/redux";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 export const Providers = (props: React.PropsWithChildren) => {
-  return <Provider store={reduxStore}>{props.children}</Provider>;
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Montserrat Variable", "sans-serif"].join(","),
+    },
+  });
+  return (
+    <Provider store={reduxStore}>
+
+      {/* mui provider */}
+      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+    </Provider>
+  );
 };

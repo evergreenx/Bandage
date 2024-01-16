81aea85d0c25e495cc81bd2e2009eb0804c9a7b7
ido evergeen <dicksonevergreenido@gmail.com>
2024-01-17 00:51:38 +0100

    add cart popover

--- app/components/Nav/header.tsx
+++ app/components/Nav/header.tsx
@@ -13,6 +13,7 @@ import {
   phoneIcon,
 } from "@/app/assets";
 import { ReduxState, removeFromWishlist } from "@/lib/redux";
+import { removeFromCart } from "@/lib/redux/slices/cartSlice";
 import { selectedCart } from "@/lib/redux/slices/cartSlice/selector";
 import { selectWishListCount } from "@/lib/redux/slices/wishlistSlice/selector";
 
@@ -28,16 +29,26 @@ export default function Header() {
 
   const selectCartList = useSelector(selectedCart);
   const [anchorEl, setAnchorEl] = React.useState<null>(null);
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popper" : undefined;
 
-  const handleClick = (event:any) => {
-  
+  const [anchorElCart, setAnchorElCart] = React.useState<null>(null);
+
+  const openWishListPopover = Boolean(anchorEl);
+
+  const openCartPopover = Boolean(anchorElCart);
+
+  const idCart = openCartPopover ? "simplecart-popper" : undefined;
+
+  const id = openWishListPopover ? "simple-popper" : undefined;
+
+  const handleClick = (event: any) => {
     setAnchorEl(anchorEl ? null : event.currentTarget);
   };
 
-  const dispatch = useDispatch();
+  const handleClickCart = (event: any) => {
+    setAnchorElCart(anchorElCart ? null : event.currentTarget);
+  };
 
+  const dispatch = useDispatch();
 
   // wishpopup
   function WishlistPopup({ open }: { open: boolean }) {
@@ -56,7 +67,7 @@ export default function Header() {
               fontSize: "16px",
               fontWeight: 600,
               my: "1px",
-              mb: '15px'
+              mb: "15px",
             }}
           >
             My wishlist
@@ -87,7 +98,7 @@ export default function Header() {
                     ml: "8px",
                   }}
                 >
-                  {i.title.slice(0,10)}.....
+                  {i.title.slice(0, 10)}.....
                 </Typography>
 
                 <Box
@@ -97,9 +108,16 @@ export default function Header() {
                   }}
                   onClick={() => dispatch(removeFromWishlist(i.id))}
                 >
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
-<path d="M 10 2 L 9 3 L 3 3 L 3 5 L 4.109375 5 L 5.8925781 20.255859 L 5.8925781 20.263672 C 6.023602 21.250335 6.8803207 22 7.875 22 L 16.123047 22 C 17.117726 22 17.974445 21.250322 18.105469 20.263672 L 18.107422 20.255859 L 19.890625 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 6.125 5 L 17.875 5 L 16.123047 20 L 7.875 20 L 6.125 5 z"></path>
-</svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M 10 2 L 9 3 L 3 3 L 3 5 L 4.109375 5 L 5.8925781 20.255859 L 5.8925781 20.263672 C 6.023602 21.250335 6.8803207 22 7.875 22 L 16.123047 22 C 17.117726 22 17.974445 21.250322 18.105469 20.263672 L 18.107422 20.255859 L 19.890625 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 6.125 5 L 17.875 5 L 16.123047 20 L 7.875 20 L 6.125 5 z"></path>
+                  </svg>
                 </Box>
               </Box>
             );
@@ -109,9 +127,141 @@ export default function Header() {
     );
   }
 
+  function CartPopup({ open }: { open: boolean }) {
+    const calculateCartTotal = (): number => {
+      return selectCartList.reduce((total, item) => {
+        const product = item.product;
+        const itemTotal = product.price * item.quantity;
+        return total + itemTotal;
+      }, 0);
+    };
+
+    const cartTotal = calculateCartTotal();
+    return (
+      <Popper id={idCart} open={open} anchorEl={anchorEl}>
+        <Box
+          sx={{
+            p: 3,
+            bgcolor: "background.paper",
+
+            boxShadow: 3,
+            height: "500px",
+            width: "500px",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "16px",
+              fontWeight: 600,
+              my: "1px",
+              mb: "15px",
+            }}
+          >
+            My cart
+          </Typography>
+
+          {selectCartList.length === 0 && (
+            <Typography>No product , try adding</Typography>
+          )}
+          {selectCartList.map((i) => {
+            return (
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Image
+                  src={i.product.thumbnail}
+                  alt="product"
+                  width={100}
+                  height={50}
+                />
+
+                <Typography
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: 600,
+                    ml: "8px",
+                  }}
+                >
+                  {i.product.title.slice(0, 10)}.....
+                </Typography>
+
+                <Typography
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: 600,
+                    ml: "8px",
+                  }}
+                >
+                  $ {i.product.price}
+                </Typography>
+
+                <Box
+                  sx={{
+                    ml: "10px",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => dispatch(removeFromCart(i.product.id))}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M 10 2 L 9 3 L 3 3 L 3 5 L 4.109375 5 L 5.8925781 20.255859 L 5.8925781 20.263672 C 6.023602 21.250335 6.8803207 22 7.875 22 L 16.123047 22 C 17.117726 22 17.974445 21.250322 18.105469 20.263672 L 18.107422 20.255859 L 19.890625 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 6.125 5 L 17.875 5 L 16.123047 20 L 7.875 20 L 6.125 5 z"></path>
+                  </svg>
+                </Box>
+              </Box>
+            );
+          })}
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              alignSelf: "flex-end",
+              justifySelf: "flex-end",
+
+              mt: "70px",
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: "30px",
+                fontWeight: 700,
+              }}
+            >
+              Total
+            </Typography>
+
+            <Typography
+              sx={{
+                fontSize: "30px",
+                fontWeight: 700,
+              }}
+            >
+              ${cartTotal}
+            </Typography>
+          </Box>
+        </Box>
+      </Popper>
+    );
+  }
+
   return (
     <>
-      <WishlistPopup open={open} />
+      {/* wishlist popover */}
+      <WishlistPopup open={openWishListPopover} />
+
+      {/* cartpopup popover */}
+      <CartPopup open={openCartPopover} />
 
       <Box
         sx={{
@@ -303,6 +453,7 @@ export default function Header() {
                   display: "flex",
                   alignItems: "center",
                 }}
+                onClick={handleClickCart}
               >
                 <Image src={StoreIcon} alt="searh icon" />
 
